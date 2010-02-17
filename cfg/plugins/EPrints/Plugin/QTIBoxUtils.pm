e976b158fb856fd02d708e18faf59148617782c4
Bart Nagel <bart@tremby.net>
2010-02-17 17:10:34 +0000

    fairly major restructure -- put zip logic in Utils file

--- cfg/plugins/EPrints/Plugin/QTIBoxUtils.pm
+++ cfg/plugins/EPrints/Plugin/QTIBoxUtils.pm
@@ -12,6 +12,7 @@ package EPrints::Plugin::QTIBoxUtils;
 sub new {
 }
 
+# given XML,
 # return 0 if not qti
 # return 1 if assessment item
 # return 2 if assessment test
@@ -32,6 +33,10 @@ sub is_qti {
 	return 0;
 }
 
+# return 1 if the given EPrint contains QTI
+# takes an session, EPrint id and a number
+#	1 if only single QTI files and single-item content packages are allowed
+#	0 if any QTI is allowed
 sub contains_qti {
 	my ($session, $eprintid, $singleonly) = @_;
 	my $eprint = new EPrints::DataObj::EPrint($session, $eprintid);
@@ -78,13 +83,104 @@ sub contains_qti {
 	}
 
 	if ($document->get_value("main") =~ /.xml$/i) {
-		open(FILE, $document->local_path() . "/" . $document->get_value("main")) or die "Couldn't open file: $!";
+		open(FILE, $document->local_path() . "/" . $document->get_value("main")) or die("qtibox: error opening file: $!");
 		my $xml = join("", <FILE>);
 		close FILE;
 		my $type = is_qti($xml, $singleonly);
 		if (!$singleonly && $type || $singleonly && $type == 1) {
 			return 1;
 		}
+	}
+
+	return 0;
+}
+
+# return XML from an EPrints document containing QTI
+# parameters are an EPrints document object and a number
+#	1 for assessmentItem XML
+#	2 for assessmentTest XML
+# returns the requested XML or 0 if it's not found. when looking for an
+# assessmentItem, 0 is returned if there is more than one (it's not a
+# single-item content package)
+sub qti_get_xml {
+	my ($doc, $type) = @_;
+
+	if ($type != 1 && $type != 2) {
+		die("qtibox: expected 1 or 2 as second argument, got '$type'\n");
+	}
+
+	if ($doc->get_value("main") =~ /\.xml$/) {
+		# it's XML. check if it's what we're looking for
+		open my $fh, '<', $doc->local_path() . "/" . $doc->get_value("main") or die("qtibox: error opening file: $!");
+		my $data = do { local $/; <$fh> };
+
+		if (is_qti($data) == $type) {
+			return $data;
+		}
+		return 0;
+	}
+
+	if ($doc->get_value("main") =~ /\.zip$/) {
+		# it's a zip file. could be a content package
+
+		my $zip = Archive::Zip->new($doc->local_path() . "/" . $doc->get_value("main"));
+		my $manifestxml = $zip->contents("imsmanifest.xml");
+		if (!defined $manifestxml) {
+			print STDERR "qtibox: error getting manifest\n";
+			return 0;
+		}
+		my $dom = eval {
+			EPrints::XML::parse_xml_string($manifestxml);
+		};
+		if ($@) {
+			my $err = $@;
+			$err =~ s# at /.*##;
+			print STDERR "qtibox: error parsing manifest XML\n";
+			return 0;
+		}
+
+		# resources
+		my $resources = $dom->getElementsByTagName("resource");
+		if ($type == 1 && $resources->getLength > 1) {
+			# want a single item but there is more than one
+			return 0;
+		} elsif ($resources->getLength == 0) {
+			print STDERR "qtibox: no resources found\n";
+			return 0;
+		}
+
+		# count tests and items
+		my @tests = ();
+		my @items = ();
+		for (my $i = 0; $i < $resources->getLength; $i++) {
+			my $t = $resources->item($i)->getAttributeNode("type")->getValue;
+			if ($t =~ /^imsqti_item_/) {
+				push(@items, $i);
+			} elsif ($t =~ /^imsqti_(test|assessment)_/) {
+				push(@tests, $i);
+			}
+		}
+
+		my @array;
+		if ($type == 1) {
+			@array = @items;
+		} else {
+			@array = @tests;
+		}
+
+		if ($array != 1) {
+			print STDERR "qtibox: wanted an assessment item or test, expected 1 to be present, found $array\n";
+			return 0;
+		}
+
+		# return the XML it points to
+		my $xmlfilename = $resources->item(pop(@array))->getAttributeNode("href")->getValue;
+		my $xml = $zip->contents($xmlfilename);
+		if (!defined $xml) {
+			print STDERR "qtibox: error getting contents of file '$xmlfilename'\n";
+			return 0;
+		}
+		return $xml;
 	}
 
 	return 0;
